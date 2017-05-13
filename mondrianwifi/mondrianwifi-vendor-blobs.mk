534608576af0024e1042726aa61217229a5f3d46
TboneWiz <netfluke@yahoo.com>
2017-05-13 12:08:44 -0400

    Remove aptXHD (not supported for mondrianwifi)

--- mondrianwifi/mondrianwifi-vendor-blobs.mk
+++ mondrianwifi/mondrianwifi-vendor-blobs.mk
@@ -244,5 +244,4 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libmm-abl.so:system/vendor/lib/libmm-abl.so \
 	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libmm-abl-oem.so:system/vendor/lib/libmm-abl-oem.so \
 	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libaptX-1.0.0-rel-Android21-ARMv7A.so:system/vendor/lib/libaptX-1.0.0-rel-Android21-ARMv7A.so \
-	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libaptXHD-1.0.0-rel-Android21-ARMv7A.so:system/vendor/lib/libaptXHD-1.0.0-rel-Android21-ARMv7A.so \
 	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libaptXScheduler.so:system/vendor/lib/libaptXScheduler.so
